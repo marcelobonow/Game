fa75982c94b92512aad58209391f8d88f80846cf
marcelobonow <marcelobonowott@hotmail.com>
2017-04-08 14:08:49 -0300

    Move Behaviour

---
+++ Assets/Scripts/Classes.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Classes : MonoBehaviour {
+    
+    //the classes the game will have goes here
+}
---
+++ Assets/Scripts/MoveBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class MoveBehaviour : MonoBehaviour {
+
+    private Rigidbody rb;
+    private Vector2 oldMousePosition;
+
+    void Update () {
+        //normally the velocity is zero, if any button was pressed
+        //the velocity changes.
+        rb.velocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.A))
+            rb.velocity = new Vector3(-10, rb.velocity.y, rb.velocity.z);
+        if(Input.GetKey(KeyCode.D))
+            rb.velocity = new Vector3(10, rb.velocity.y, rb.velocity.z);
+        if (Input.GetKey(KeyCode.W))
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 10);
+        if (Input.GetKey(KeyCode.S))
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.z, -10);
+    }
+}
